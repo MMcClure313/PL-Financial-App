33f6ed3029cb7b2d19f2fce9003507d47971bb1f
MMcClure313 <mpm35@students.uwf.edu>
2025-04-22 18:38:54 -0500

    Adds in file creation. The csv files are ignored with a .gitignore

--- python/login.py
+++ python/login.py
@@ -1,3 +1,5 @@
+import os
+
 USERS_FILE = "users.txt"
 
 # Tries to read a users file.
@@ -20,12 +22,20 @@ def save_user(user, password):
     except FileNotFoundError:
         pass
 
+# Creates the financial data file for the user and adds in some headers.
+def create_user_csv(username):
+    filename = f"{username}.csv"
+    if not os.path.exists(filename):
+        with open(filename, "w") as f:
+            f.write("Date,Description,Amount,Category\n")  # headers for financial data
+
 # Checks if the user exists first, then saves user based on params
 def register_user(username, password):
     users = load_users()
     if username in users:
         return False, "User already exists."
     save_user(username, password)
+    create_user_csv(username)
     return True, "Registered successfully!"
 
 # Just checks if username and password match. Still hate this whole double return thing.
@@ -36,3 +46,5 @@ def login_user(username, password):
     if users[username] != password:
         return False, "Incorrect password."
     return True, "Login successful!"
+
+
